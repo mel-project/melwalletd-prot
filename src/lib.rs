4a5819089d5df86c86796f651ab12e8b2126b0f8
nullchinchilla <nullchinchilla@pm.me>
2022-10-30 15:08:05 -0500

    readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+//! Crate defining the [nanorpc] (JSON-RPC 2.0) protocol exposed by melwalletd, the reference Themelio wallet microservice implementation.
+//!
+//! As a user, you almost certainly only care about [MelwalletdClient], which you would generally want to wrap around an HTTP-based [nanorpc::RpcTransport] in order to talk to melwalletd.
 mod protocol;
 pub use protocol::*;
-pub mod error;
-pub mod signer;
+
 pub mod types;
-pub mod walletdata;
+// pub use types::*;
